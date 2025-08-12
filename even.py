b57df4a4c7b5332ac036d16a166efb45fd2c0ed1
Aniruddh Khatri <aniruddhkhatri68@gmail.com>
2025-08-12 15:48:21 +0530

    Initial API setup

--- even.py
+++ even.py
@@ -1,3 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from your API!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
 class Numbers:
     def __init__(self,n):
         self.n = n
